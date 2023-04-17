1645a9d5ec8945b2328ae2d9999be034efd4c4ee
Sigit Haryoyudanto <83746683+vvarl0cks@users.noreply.github.com>
2023-04-17 17:21:51 +0800

    fecth 0.svg

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -426,7 +426,7 @@ export default function Home() {
           }
         </div>
         <div>
-          <img className={styles.image} src="/cryptodevs/0.svg" />
+          <img className={styles.image} src="public/cryptodevs/0.svg" />
         </div>
       </div>
 
