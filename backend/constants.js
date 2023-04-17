9a599c6bce155e9aca016416dcb2b813932c569b
Sigit Haryoyudanto <hello@sigit.co.uk>
2023-04-17 05:53:52 +0000

    first commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x11dDE0431A6ecD4d571a6bcb14318E8427ECb0f4";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
